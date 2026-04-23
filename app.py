fe82f27807746af244eb00adc8213d62184c1600
Akbar Akbarov <akbarovakbar888@gmail.com>
2026-04-23 16:54:57 +0500

    bug fix

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import time
 import gps_parse
 from can_decoder import can_reader, can_data
 
-
-
 can_thread = threading.Thread(target=can_reader, daemon=True)
 can_thread.start()
 
-
 while True:
+    print("CAN Data:", can_data)
     print(f"Lat: {gps_parse.get_latitude():.6f}, Lon: {gps_parse.get_longitude():.6f}, Speed: {gps_parse.get_speed_mph():.1f} mph, Dir: {gps_parse.get_direction()} ({gps_parse.get_degree()}°), State: {gps_parse.get_state()} ({gps_parse.get_state_code()})")
     time.sleep(1)
