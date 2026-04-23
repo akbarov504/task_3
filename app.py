add53f4b85b87923b4fb6da3c9e29a6122da07a2
Akbar Akbarov <akbarovakbar888@gmail.com>
2026-04-23 16:35:34 +0500

    bug fix

--- app.py
+++ app.py
@@ -5,20 +5,17 @@ from can_decoder import can_reader, can_data
 
 app = Flask(__name__)
 
-gps_info = {
-    "latitude": gps_parse.get_latitude(),
-    "longitude": gps_parse.get_longitude(),
-    "speed": gps_parse.get_speed_mph(),
-    "direction": gps_parse.get_direction(),
-    "degree": gps_parse.get_degree(),
-    "state": gps_parse.get_state(),
-    "state_code": gps_parse.get_state_code()
-}
-
 @app.route("/api/telemetry", methods=["GET"])
 def get_telemetry():
-    print(can_data)
-    print(gps_info)
+    gps_info = {
+        "latitude": gps_parse.get_latitude(),
+        "longitude": gps_parse.get_longitude(),
+        "speed": gps_parse.get_speed_mph(),
+        "direction": gps_parse.get_direction(),
+        "degree": gps_parse.get_degree(),
+        "state": gps_parse.get_state(),
+        "state_code": gps_parse.get_state_code()
+    }
 
     return jsonify({
         "can": can_data,
